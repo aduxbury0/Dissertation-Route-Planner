e413d44ff5236fe0b473be2877dc55fb610677c1
Alex <duxburya@uni.coventry.ac.uk>
2019-03-07 20:37:21 +0000

    Initial Commit, includes API Module

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(200);
+});
+
+const PORT = process.env.port || 8000;
+
+app.listen(PORT, () => console.log(`Server running on port PORT`));
