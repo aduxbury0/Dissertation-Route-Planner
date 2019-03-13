24a7d63cc1f054a8921b558a5101199dc84b2fc8
Alex <duxburya@uni.coventry.ac.uk>
2019-03-13 02:15:31 +0000

    converted all to promises

--- modules/apiRequests.js
+++ modules/apiRequests.js
@@ -26,48 +26,69 @@ function heightCall(lat, long) {
 	});
 }
 
+/**
+ * Returns a JSobject of information about weather at a specified location
+ * @async
+ * @param {float} lat - latitude of weather area
+ * @param {float} long - longitude of weather area
+ * @returns {JSObject} = A JSObject of info about weather
+ */
 function weatherCall(lat, long) {
+	return new Promise((resolve, reject) => {
 
-	axios({
-		method: 'get',
-		url: 'https://weather.cit.api.here.com/weather/1.0/report.json',
-		data: {
-			product: 'observation',
-			latitude: lat,
-			longitude:  long,
-			oneobservation: 'true',
-			app_id: 'hF9FhuQZC0D6ix1haZeZ',
-			app_code: 'sc3s5Gr4xLP1v3d2uRsciQ' 
-		}
-	})
-		.then((weather) => {
-			const weatherReport = {
-				"precipitation1H": weather.observations.location.obervation.precipitation1H,
-				"precipitation3H": weather.observations.location.obervation.precipitation3H,
-				"precipitation6H": weather.observations.location.obervation.precipitation6H,
-				"precipitation12H": weather.observations.location.obervation.precipitation12H,
-				"precipitation24H": weather.observations.location.obervation.precipitation24H,
-				"windSpeed": weather.observations.location.obervation.windSpeed,
-				"windDirection": weather.observations.location.obervation.windDirection,
-				"windDescShort": weather.observations.location.obervation.windDescShort
+		axios({
+			method: 'get',
+			url: 'https://weather.cit.api.here.com/weather/1.0/report.json',
+			data: {
+				product: 'observation',
+				latitude: lat,
+				longitude:  long,
+				oneobservation: 'true',
+				app_id: 'hF9FhuQZC0D6ix1haZeZ',
+				app_code: 'sc3s5Gr4xLP1v3d2uRsciQ' 
 			}
-
-			return weatherReport;
-
 		})
-		.catch(err => console.log(err));
-
+			.then((weather) => {
+				const weatherReport = {
+					"precipitation1H": weather.observations.location.obervation.precipitation1H,
+					"precipitation3H": weather.observations.location.obervation.precipitation3H,
+					"precipitation6H": weather.observations.location.obervation.precipitation6H,
+					"precipitation12H": weather.observations.location.obervation.precipitation12H,
+					"precipitation24H": weather.observations.location.obervation.precipitation24H,
+					"windSpeed": weather.observations.location.obervation.windSpeed,
+					"windDirection": weather.observations.location.obervation.windDirection,
+					"windDescShort": weather.observations.location.obervation.windDescShort
+				}
+	
+				resolve(weatherReport);
+	
+			})
+			.catch(err => reject(console.log(err)));
+	});
 }
 
+/**
+ * Returns a JSON object containing all flights within bounded area
+ * @async
+ * @param {float} latMin 
+ * @param {float} longMin 
+ * @param {float} latMax 
+ * @param {float} longMax 
+ * @returns {JSON}	
+ */
 function airspaceCall(latMin, longMin, latMax, longMax) {
-	axios({
-		method: 'get',
-		url: `https://opensky-network.org/api/states/all?lamin=${latMin}&lomin=${longMin}&lamax=${latMax}&lomax=${longMax}`
-	})
-		.then((flights) => {
-			return flights.states;
+	return new Promise((reject, resolve) => {
+
+		axios({
+			method: 'get',
+			url: `https://opensky-network.org/api/states/all?lamin=${latMin}&lomin=${longMin}&lamax=${latMax}&lomax=${longMax}`
 		})
-		.catch(err => console.log(err));
+			.then((flights) => {
+				resolve(flights.states);
+			})
+			.catch(err => reject(console.log(err)));
+	});
+	
 
 }
 
